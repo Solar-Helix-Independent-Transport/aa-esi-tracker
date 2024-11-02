75555896135bbe3ba5fc04ff04938f1ba21a9ac9
Aaron Kable <aaronkable@gmail.com>
2024-11-02 13:39:38 +0800

    different import/export bulk create

--- esi_tracker/management/commands/es_import.py
+++ esi_tracker/management/commands/es_import.py
@@ -10,11 +10,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write("Importing All of ESIT updates to CSV from esit.csv")
+        self.stdout.write("Clearing Old Data.")
         
+        ESIEndpointStatus.objects.all().delete()
+        ESIEndpoint.objects.all().delete()
+        self.stdout.write("Starting.")
+
         count_ep = 0
         count_new_ep = 0
         count_data = 0
-        count_new_data = 0
 
         with open("esit-e.csv", 'r') as myfile:
             wr = csv.DictReader(myfile)
@@ -48,23 +52,23 @@ class Command(BaseCommand):
             self.stdout.write(f"Total Rows to import {total}")
             updated = 0
 
+            ops = []
             for row in wr:
-                _eps, _eps_created = ESIEndpointStatus.objects.get_or_create(
+                ops.append(ESIEndpointStatus(
                     endpoint_id=row["epid"],
                     date=datetime.fromisoformat(row["date"]),
                     status=row["status"]
-                )
+                ))
                 count_data += 1
-                if _eps_created:
-                    count_new_data += 1
                 if count_data > updated+4999:
-                    self.stdout.write(f"Completed: {count_data}({count_new_data})/{total}")
+                    self.stdout.write(f"Completed: {count_data}/{total}")
                     updated = count_data
 
+            ESIEndpointStatus.objects.bulk_create(ops)
 
         self.stdout.write(
             (
-                f"imported data:{count_data:,} new:{count_new_data:,}"
+                f"imported data:{count_data:,}"
                 " rows successfully to esit-s.csv!"
             )
         )
